f16420a0bfc25cf88482cc3d8e1725d0087afc10
Tsuginosuke Sakauchi <dev@fairlandcircle.com>
2016-03-25 23:10:19 -0500

    feat: Add initial entries test suite

--- src/jasmine/spec/feedreader.js
+++ src/jasmine/spec/feedreader.js
@@ -111,15 +111,32 @@ $(function() {
 
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Initial entries test suite */
+    describe('Initial entries', function() {
 
-        /* TODO: Write a test that ensures when the loadFeed
+        var feed, entry;
+
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        /* Test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
+        it('has at least a single .entry element within the .feed container.', function() {
+            feed = $('.feed');
+            entry = feed.find(".entry");
+            expect(entry).toBeDefined();
+            expect(entry).not.toBeNull();
+            expect(entry.length).not.toBe(0);
+        });
+    });
+    
     /* TODO: Write a new test suite named "New Feed Selection"
 
         /* TODO: Write a test that ensures when a new feed is loaded
